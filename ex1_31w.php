c721085d6ecc487f968c89dcdb6440a060b5b5e1
Olena <e2195610@cmaisonneuve.qc.ca>
2022-06-23 23:07:12 -0400

    Ajout du enqueue_style dans le plugin

--- ex1_31w.php
+++ ex1_31w.php
@@ -3,9 +3,20 @@
  * Plugin Name: Extension de l'exercice #1
  * Author: Olena Khliupina
  * Description: Une première extension pour l'exercice #1
- * Author URI: https://github.com/sabrinoushka
+ * Author URI: https://github.com/sabrinoushka/31w-extension1
  */
 ?>
-
+   
 <?php
+    function fonction_generale(){
+        function mon_31w_enqueue(){
+            wp_enqueue_style(
+                'mon_31w-style',
+                get_stylesheet_uri(),
+                array(),
+                _S_VERSION);            
+        }
+       add_action('wp_enqueue_scripts', 'mon_31w_enqueue');
+    }
+     add_action('init', "fonction_generale");
 ?>
