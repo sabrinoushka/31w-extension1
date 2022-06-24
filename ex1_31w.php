8ef1de6f365fdbfcc311e815fcead4bf0b308594
Olena <e2195610@cmaisonneuve.qc.ca>
2022-06-23 22:20:17 -0400

    Configuration de notre premier plugin

---
+++ ex1_31w.php
@@ -0,0 +1,11 @@
+<?php   
+/**
+ * Plugin Name: Extension de l'exercice #1
+ * Author: Olena Khliupina
+ * Description: Une première extension pour l'exercice #1
+ * Author URI: https://github.com/sabrinoushka
+ */
+?>
+
+<?php
+?>
